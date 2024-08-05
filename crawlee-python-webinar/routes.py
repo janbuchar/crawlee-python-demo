c17030dd3574938b1dad771968e41327f5fb1d7e
Jan Buchar <Teyras@gmail.com>
2024-08-05 09:49:11 +0200

    feat: Enqueue shoe listing links

--- crawlee-python-webinar/routes.py
+++ crawlee-python-webinar/routes.py
@@ -1,4 +1,7 @@
+import asyncio
+
 from crawlee.basic_crawler import Router
+from crawlee.models import Request
 from crawlee.playwright_crawler import PlaywrightCrawlingContext
 
 router = Router[PlaywrightCrawlingContext]()
@@ -10,3 +13,19 @@ async def default_handler(context: PlaywrightCrawlingContext) -> None:
 
     # Wait for the popup to be visible to ensure it has loaded on the page.
     await context.page.get_by_test_id('dialog-accept-button').click()
+
+    shoe_listing_links = (
+        await context.page.get_by_test_id('link').filter(has_text='All shoes').all()
+    )
+    await context.add_requests(
+        [
+            Request.from_url(url, label='listing')
+            for link in shoe_listing_links
+            if (url := await link.get_attribute('href'))
+        ]
+    )
+
+
+@router.handler('listing')
+async def listing_handler(context: PlaywrightCrawlingContext) -> None:
+    """Handler for shoe listings."""
